82f00fb8b737a4103cc38c84779fd337d5f5a65b
Muh-Baharuddin <4519210022@univpancasila.ac.id>
2023-09-24 12:18:53 +0700

    add denon as hot reload

--- auth/auth.controller.ts
+++ auth/auth.controller.ts
@@ -1,11 +1,14 @@
-import { Router } from "https://deno.land/x/oak/mod.ts";
+import { Router } from "https://deno.land/x/oak@v12.6.1/mod.ts";
 
-const authRouter = new Router()
+const authRouter = new Router();
 
 authRouter
   .get("/", (context) => {
-    context.response.body = "Hello auth!"
-    console.log( "context", context)
+    context.response.body = "Hello auth!";
+    console.log("context", context);
   })
+  .post("/login", (context) => {
+    context.response.body = "This is a POST request!";
+  });
 
 export default authRouter;
