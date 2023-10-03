e24c491903d97faad5a3024607c4a09a233bf86f
Muh-Baharuddin <4519210022@univpancasila.ac.id>
2023-10-03 12:20:39 +0700

    add id in address interface

---
+++ users/users.types.ts
@@ -0,0 +1,19 @@
+export interface User {
+  _id?: string;
+  email: string;
+  username: string;
+  addresses?: Address[];
+  password?: string;
+}
+
+export interface Address {
+  _id: string;
+  reciever: string;
+  contact : string;
+  streetName: string;
+  subdistrict: string;
+  city: string;
+  postalCode: string;
+  description: string;
+  coordinate: string;
+}
