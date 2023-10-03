1c53416ed89177d85f90255a0d4d351c74fbd71c
Muh-Baharuddin <4519210022@univpancasila.ac.id>
2023-10-03 10:01:11 +0700

    create new route add user addresses

--- users/users.controller.ts
+++ users/users.controller.ts
@@ -1,19 +1,25 @@
 import { Router } from "$oak/mod.ts";
 import { validate } from "/middlewares/validate.ts";
-import { edit, getAll, getUserProfile, removeUser } from "./users.service.ts";
-import { UserProfile } from "/auth/auth.types.ts";
+import { addAddress, edit, getAll, getUserProfile, removeUser } from "./users.service.ts";
 import { authMiddleware } from "/middlewares/jwt.ts";
 import { AppContext } from "/utils/types.ts";
-import { userValidate } from "/users/users.validation.ts";
+import { addressValidate, userValidate } from "/users/users.validation.ts";
+import { Address, User } from "/users/user.types.ts";
 
 const usersRouter = new Router();
 
 usersRouter
-  .get("/", authMiddleware ,async (context): Promise<UserProfile[]> => {
+  .get("/", authMiddleware ,async (context): Promise<User[]> => {
     const allData = await getAll()
     return context.response.body = allData;
   })
-  .get("/profile", authMiddleware ,async (context: AppContext): Promise<UserProfile | undefined> => {
+  .get("/profile", authMiddleware ,async (context: AppContext): Promise<User | undefined> => {
+    const userid = context?.user?._id!;
+
+    const userProfile = await getUserProfile(userid)
+    return context.response.body = userProfile;
+  })
+  .get("/profile", authMiddleware ,async (context: AppContext): Promise<User | undefined> => {
     const userid = context?.user?._id!;
 
     const userProfile = await getUserProfile(userid)
@@ -22,17 +28,23 @@ usersRouter
   .put("/profile", authMiddleware, validate(userValidate), async(context: AppContext) => {
     const userid = context?.user?._id!;
 
-    const userData: UserProfile = await context.request.body().value;
+    const userData: User = await context.request.body().value;
 
     const updateData = await edit(userData, userid, context);
 
     return context.response.body = updateData;
   })
-  .delete("/profile", authMiddleware, async(context: AppContext) => {
+  .delete("/profile", authMiddleware, async(context: AppContext) : Promise<{ message: string}> => {
     const userid = context?.user?._id!;
     const deleted = await removeUser(userid, context);
 
     return context.response.body = deleted;
   })
+  .post("/address", authMiddleware, validate(addressValidate), async (context: AppContext) : Promise<{ message: string}> => {
+    const address: Address = await context.request.body().value;
+    const userid = context?.user?._id!;
+    const newAddress = await addAddress(address, userid, context);
+    return context.response.body = newAddress;
+  });
 
 export default usersRouter;
