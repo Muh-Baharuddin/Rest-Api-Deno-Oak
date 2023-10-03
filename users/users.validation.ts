db0e18f5aec8274c11ddfd138eb5bac5c35cd4bc
Muh-Baharuddin <4519210022@univpancasila.ac.id>
2023-10-03 10:02:28 +0700

    create  address validation

--- users/users.validation.ts
+++ users/users.validation.ts
@@ -4,3 +4,14 @@ export const userValidate = z.object({
   email: z.string().email(),
   username: z.string().min(3),
 })
+
+export const addressValidate = z.object({
+  reciever: z.string().min(3),
+  contact: z.string().min(10),
+  streetName: z.string().min(3),
+  subdistrict: z.string().min(3),
+  city: z.string().min(3),
+  postalCode: z.string().min(6),
+  description: z.string().min(3),
+  coordinate: z.string().min(3),
+});
