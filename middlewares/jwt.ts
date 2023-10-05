0828cc08dd301c2d0288ff612c63a69722b52cd4
Muh-Baharuddin <4519210022@univpancasila.ac.id>
2023-10-05 10:50:45 +0700

    use decode rather than verify

--- middlewares/jwt.ts
+++ middlewares/jwt.ts
@@ -1,17 +1,62 @@
-import { verify } from "$djwt/mod.ts";
+import { decode, verify } from "$djwt/mod.ts";
 import { Next } from "$oak/mod.ts";
 import { User } from "../users/users.types.ts";
 import { key } from "/utils/jwt.ts";
 import { AppContext } from "/utils/types.ts";
 
+// export const authMiddleware = async (ctx: AppContext, next: Next) => {
+//   const token = ctx.request.headers.get("Authorization");
+//   console.log("token", token)
+//   try {
+//     const data = await verify(token?.split(" ")[1]!, key);
+//     console.log("data", data)
+//     ctx.user = data.user as User;
+//   } catch(_err) {
+//     console.log("INI ERROR", _err)
+//     ctx.response.body = "Forbidden";
+//     ctx.throw(403);
+//   }
+//   await next();
+// };
+
 export const authMiddleware = async (ctx: AppContext, next: Next) => {
   const token = ctx.request.headers.get("Authorization");
   try {
-    const data = await verify(token?.split(" ")[1]!, key);
-    ctx.user = data.user as User;
+    const [_header, payload, _signature] = decode(token?.split(" ")[1]!);
+    const userPayload = payload as AppContext
+    ctx.user = userPayload.user as User;
   } catch(_err) {
+    console.log("INI ERROR", _err)
     ctx.response.body = "Forbidden";
     ctx.throw(403);
   }
   await next();
 };
+
+// export const authMiddleware = async (ctx: AppContext, next: Next) => {
+//   try {
+//     const authorization  = ctx.request.headers.get("Authorization");
+//     console.log("authorization", authorization )
+    
+//     if(!authorization) {
+//       ctx.throw(401);
+//     }
+//     const token  = authorization.split(' ')[1];
+    
+//     if(!token) {
+//       ctx.throw(401);
+//     }
+
+//     const payload = await verify(token, key);
+//     console.log("payload", payload)
+//     if(!payload){
+//       throw new Error("!payload")
+//     }
+//     ctx.user = payload.user as User;
+//   } catch(_err) {
+//     console.log("INI ERROR", _err)
+//     ctx.response.body = "Forbidden";
+//     ctx.throw(403);
+//   }
+//   await next();
+// };
