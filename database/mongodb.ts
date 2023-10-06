9e4ebd1971c7ce6499236c80c59b0a73eaf923d2
Muh-Baharuddin <4519210022@univpancasila.ac.id>
2023-10-06 14:01:51 +0700

    test deploy

--- database/mongodb.ts
+++ database/mongodb.ts
@@ -8,7 +8,8 @@ await client.connect(
   env["DATABASE_CONNECTION"],
 );
 
-export const db = client.database(env["DATABASE_NAME"]);
+// export const db = client.database(env["DATABASE_NAME"]);
+export const db = client.database("martchi");
 
 export default client;
 
