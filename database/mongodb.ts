e791e2733c6d9723ece25977632166c75e399ea7
Muh-Baharuddin <4519210022@univpancasila.ac.id>
2023-10-06 14:03:32 +0700

    test deploy

--- database/mongodb.ts
+++ database/mongodb.ts
@@ -1,11 +1,12 @@
 import { MongoClient } from "$mongo/mod.ts";
-import { load } from "$dotenv/mod.ts";
+// import { load } from "$dotenv/mod.ts";
 
 const client = new MongoClient();
-const env = await load();
+// const env = await load();
 
 await client.connect(
-  env["DATABASE_CONNECTION"],
+  // env["DATABASE_CONNECTION"],
+  "mongodb+srv://aldi:12345@testing-indonesia.uassntj.mongodb.net/martchi?authMechanism=SCRAM-SHA-1",
 );
 
 // export const db = client.database(env["DATABASE_NAME"]);
