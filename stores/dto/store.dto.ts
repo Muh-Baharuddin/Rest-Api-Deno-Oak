3e139a0af16f02ec725bd6566a43436885f74418
Muh-Baharuddin <4519210022@univpancasila.ac.id>
2023-10-12 15:43:54 +0700

    use dto for store

---
+++ stores/dto/store.dto.ts
@@ -0,0 +1,14 @@
+import { z } from "$zod/mod.ts";
+import { personValidate } from "/person/dto/person.dto.ts";
+import { addressValidate } from "/address/dto/address.dto.ts";
+
+export const storeValidate = z.object({
+  name: z.string().min(3),
+  address: addressValidate,
+  description: z.string().min(3),
+  owner: z.optional(z.string().min(3)),
+  person: z.optional(personValidate),
+});
+
+export type StoreDto = z.infer<typeof storeValidate>;
+
