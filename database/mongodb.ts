f76ab2b682a0c9cd63119c2a3fbb4b9e1ab91e4b
Muh-Baharuddin <4519210022@univpancasila.ac.id>
2023-10-14 20:41:23 +0700

    delete comment

--- database/mongodb.ts
+++ database/mongodb.ts
@@ -4,13 +4,11 @@ import { config } from "$dotenv/mod.ts";
 config({export: true});
 
 const client = new MongoClient();
-// const env = await load();
 
 await client.connect(
   Deno.env.get("DATABASE_CONNECTION")!
 );
 
-// export const db = client.database(env["DATABASE_NAME"]);
 export const db = client.database(Deno.env.get("DATABASE_NAME")!);
 
 export default client;
