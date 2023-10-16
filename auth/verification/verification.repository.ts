7ee29855ec01ccbb28c2880e95497ab603e42b1b
Muh-Baharuddin <4519210022@univpancasila.ac.id>
2023-10-16 14:05:08 +0700

    create verification repository

---
+++ auth/verification/verification.repository.ts
@@ -0,0 +1,33 @@
+import { db } from "/database/mongodb.ts";
+import { Verification } from "/auth/verification/verification.types.ts";
+
+
+const verificationCollection =  db.collection<Verification>("verification");
+
+verificationCollection.createIndexes({
+  indexes: [
+    {
+      key: {
+        "email": 1,
+      },
+      name: "index_email"
+    }
+  ]
+})
+
+export const insertVerif = async (verifData: Verification) => {
+  await verificationCollection.insertOne(verifData);
+
+  return verifData;
+}
+
+export const findCode = async (email: string, verificationCode: string) => {
+  const find = await verificationCollection.findOne(
+    { 
+      email,
+      verificationCode
+    }
+  )
+  console.log("find", find)
+  return find;
+}
