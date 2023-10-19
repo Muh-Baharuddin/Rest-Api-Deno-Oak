c491de41ad712178705d87daf8d2ec7fb033d9b5
Muh-Baharuddin <4519210022@univpancasila.ac.id>
2023-10-19 12:42:31 +0700

    fix type of field created_by and updated_by to person

--- categories/categories.types.ts
+++ categories/categories.types.ts
@@ -1,10 +1,11 @@
 import { ObjectId } from "$mongo/mod.ts";
+import { Person } from "/users/person/person.types.ts";
 
 export interface Category {
   _id: ObjectId;
   name: string;
   created_at: Date;
   updated_at: Date;
-  created_by?: string;
-  updated_by?: string;
+  created_by: Person;
+  updated_by: Person;
 }
