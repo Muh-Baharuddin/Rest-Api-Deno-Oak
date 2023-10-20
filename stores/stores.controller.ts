9c86bdaa0f3bb489486f154a316892775426709b
Muh-Baharuddin <4519210022@univpancasila.ac.id>
2023-10-20 11:24:12 +0700

    fix add and update store

--- stores/stores.controller.ts
+++ stores/stores.controller.ts
@@ -2,7 +2,7 @@ import { Router } from "$oak/mod.ts";
 import { validate } from "/middlewares/validate.ts";
 import { authMiddleware } from "/middlewares/jwt.ts";
 import { Store } from "./stores.types.ts";
-import { storeValidate } from "./dto/store.dto.ts";
+import { StoreDto, storeValidate } from "./dto/store.dto.ts";
 import { addStore, findAllStore, findStoreById, removeStore, updateStore } from "./stores.service.ts";
 
 const storesRouter = new Router();
@@ -18,8 +18,8 @@ storesRouter
     return context.response.body = store;
   })
   .post("/", authMiddleware, validate(storeValidate), async (context): Promise<{ message: string}> => {
-    const store: Store = await context.request.body().value;
-    const newStore = await addStore(store);
+    const store: StoreDto = await context.request.body().value;
+    const newStore = await addStore(store, context);
     return context.response.body = newStore;
   })
   .put("/:id", authMiddleware, validate(storeValidate), async (context) : Promise<{ message: string}> => {
