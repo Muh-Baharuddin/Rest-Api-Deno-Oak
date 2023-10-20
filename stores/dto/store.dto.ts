7a3cf6824b69da175be1946c6a6da3ac0c7e8b3f
Muh-Baharuddin <4519210022@univpancasila.ac.id>
2023-10-20 13:18:24 +0700

    fix store validate

--- stores/dto/store.dto.ts
+++ stores/dto/store.dto.ts
@@ -1,10 +1,7 @@
 import { z } from "$zod/mod.ts";
-import { addressValidate } from "/users/address/dto/address.dto.ts";
 
 export const storeValidate = z.object({
   name: z.string().min(3),
-  owner: z.string().min(3),
-  address: addressValidate,
   description: z.string().min(3),
 });
 
