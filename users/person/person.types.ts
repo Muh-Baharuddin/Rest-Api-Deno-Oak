4d73dfd8c8114afb28b34118752c206691f9e185
Muh-Baharuddin <4519210022@univpancasila.ac.id>
2023-10-21 12:41:05 +0700

    change type bod to date only

--- users/person/person.types.ts
+++ users/person/person.types.ts
@@ -3,7 +3,7 @@ import { ObjectId } from "$mongo/mod.ts";
 export interface Person {
   _id: ObjectId;
   name: string;
-  bod: Date | string;
+  bod: Date;
   phoneNumber: string;
   ktp?: string;
   npwp?: string;
