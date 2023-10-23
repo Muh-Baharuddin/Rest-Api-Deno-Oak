164a214bf544b20d194f26e71bd6a5ef99c5ba0e
Muh-Baharuddin <4519210022@univpancasila.ac.id>
2023-10-23 12:07:47 +0700

    created by and updated by type as user

--- items/items.types.ts
+++ items/items.types.ts
@@ -1,6 +1,6 @@
 import { ObjectId } from "$mongo/mod.ts";
-import { Person } from "/users/person/person.types.ts";
 import { Category } from "/categories/categories.types.ts";
+import { User } from "/users/users.types.ts";
 
 export interface Item {
   _id: ObjectId;
@@ -9,6 +9,6 @@ export interface Item {
   category: Category[];
   created_at: Date;
   updated_at: Date;
-  created_by: Person;
-  updated_by: Person;
+  created_by: User;
+  updated_by: User;
 }
