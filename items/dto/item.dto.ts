d72aa9fdb71698ef1aad42cb167c6cd5a29762d5
Muh-Baharuddin <4519210022@univpancasila.ac.id>
2023-10-30 11:50:21 +0700

    add image field in validate

--- items/dto/item.dto.ts
+++ items/dto/item.dto.ts
@@ -5,7 +5,8 @@ export const itemValidate = z.object({
   price: z.number().positive(),
   category: z.array(z.object({
     name: z.string().min(2)
-  }))
+  })),
+  image: z.array(z.string().min(2)).min(1),
 })
 
 export type ItemDto = z.infer<typeof itemValidate>;
